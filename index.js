9626d49f1bc3243cdc969ba2c73eec3608493626
1SJulioS1 <91168844+1SJulioS1@users.noreply.github.com>
2024-02-04 11:41:42 -0600

    initial commit

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const { connectToDatabase } = require("./config/dbConn.js");
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(cors());
+app.use(express.static("public"));
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+app.post("/api/users", async (req, res) => {
+  const db = await connectToDatabase();
+  const collection = db.collection("Users");
+  if (!req?.body?.username) {
+    return res.json({ message: "No username provided" });
+  }
+  const duplicate = await collection.findOne({ username: req.body.username });
+  if (duplicate) {
+    return res.json({
+      username: req.body.username,
+      _id: duplicate._id,
+    });
+  } else {
+    const result = await collection.insertOne({
+      username: req.body.username,
+    });
+    return res.json({
+      username: req.body.username,
+      _id: result.insertedId.toString(),
+    });
+  }
+});
 
-
-
+app.get("/api/users", async (req, res) => {
+  const db = await connectToDatabase();
+  const collection = db.collection("Users");
+  const users = await collection.find().toArray();
+  return res.json(users);
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
