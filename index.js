94f5faebd9467721be40701b23079dc138551ef1
1SJulioS1 <91168844+1SJulioS1@users.noreply.github.com>
2024-02-05 10:20:41 -0600

    id validation

--- index.js
+++ index.js
@@ -54,30 +54,54 @@ app.post("/api/users/:_id/exercises", async (req, res) => {
   if (!req?.params?._id || !req?.body?.description || !req?.body?.duration) {
     return res.status(400).json({ message: "Form data needed" });
   }
-  const result = await user.findOne({
-    _id: new ObjectId(req.params._id),
-  });
-  if (result) {
-    const date = !req?.body?.date
-      ? new Date().toDateString()
-      : new Date(req.body.date).toDateString();
-    const newExercise = await exercise.insertOne(
-      {
-        username: result.username,
+  if (ObjectId.isValid(_id)) {
+    const result = await user.findOne({
+      _id: new ObjectId(req.params._id),
+    });
+    if (result) {
+      const date = !req?.body?.date
+        ? new Date().toDateString()
+        : new Date(req.body.date).toDateString();
+      const newExercise = await user
+        .findOneAndUpdate(
+          {
+            _id: new ObjectId(req.params._id),
+          },
+          {
+            $push: {
+              log: {
+                description: req.body.description,
+                duration: Number(req.body.duration),
+                date: date,
+              },
+            },
+            /* username: result.username,
         description: req.body.description,
         duration: parseInt(req.body.duration),
-        date,
-      },
-      { returnOriginal: true }
-    );
-    const insertedExercise = await exercise.findOne({
+        date, */
+          },
+          { new: true }
+        )
+        .then((data) => {
+          res.send({
+            username: data.username,
+            date: new Date(date).toDateString(),
+            description: req.body.description,
+            duration: Number(req.body.duration),
+            _id: id,
+          });
+        });
+      /* const insertedExercise = await exercise.findOne({
       _id: newExercise.insertedId,
     });
-    res.status(200).json(insertedExercise);
+    res.status(200).json(insertedExercise); */
+    } else {
+      return res
+        .status(404)
+        .json({ message: `No user with id: ${req.params.id}` });
+    }
   } else {
-    return res
-      .status(404)
-      .json({ message: `No user with id: ${req.params.id}` });
+    res.json({ error: "NOT VALID _ID" });
   }
 });
 
